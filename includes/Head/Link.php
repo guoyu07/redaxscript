3b4a4a69065e9f91aa4dd8e0210c2df3decee818
JimMorrison723 <bali723@gmail.com>
2017-03-18 15:41:58 +0100

    remove is_null

--- includes/Head/Link.php
+++ includes/Head/Link.php
@@ -163,7 +163,7 @@ class Link extends HeadAbstract
 		$linkElement->init('link');
 		$collectionArray = self::$_collectionArray[self::$_namespace];
 
-		if (!is_null($collectionArray))
+		if ($collectionArray !== NULL)
 		{
 			$collectionKeys = array_keys($collectionArray);
 			$lastKey = end($collectionKeys);
